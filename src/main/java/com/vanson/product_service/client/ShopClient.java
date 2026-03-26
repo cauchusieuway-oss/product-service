5fae5b0685528b4086c43fcfafffcd10e7df343a
cauchusieuway-oss <cauchusieuway@gmail.com>
2026-03-27 02:09:32 +0700

    Add URL to FeignClient for ShopService

--- src/main/java/com/vanson/product_service/client/ShopClient.java
+++ src/main/java/com/vanson/product_service/client/ShopClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "shop-service")
+@FeignClient(name = "shop-service", url = "https://shop-service-jh80.onrender.com")
 public interface ShopClient {
 
     @GetMapping("/api/shops/my-shop")
